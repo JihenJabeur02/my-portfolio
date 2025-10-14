80bfb715e946da5191151d141c460883d559ce86
JihenJabeur02 <jihenjabeur@ieee.org>
2025-10-14 21:14:04 +0100

    updated meta tags

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -61,7 +61,7 @@ export default function Hero() {
       {/* Hero content */}
       <div className="relative z-10 flex flex-col items-center text-center px-4 py-20">
         {/* Profile image */}
-        <div className="relative w-full flex justify-center mb-10">
+        <div className="relative w-full flex justify-center mb-20">
           <CircularImage color={color} />
         </div>
 
