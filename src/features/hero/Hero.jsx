11e26cac1801acd203c3f4f454c6e1f3f04d26ac
JihenJabeur02 <jihenjabeur@ieee.org>
2025-10-14 20:52:32 +0100

    updated meta tags

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -61,13 +61,13 @@ export default function Hero() {
       {/* Hero content */}
       <div className="relative z-10 flex flex-col items-center text-center px-2 py-25">
         {/* Profile image */}
-        <div className="relative w-full flex justify-center mb-10">
+        <div className="relative w-full flex justify-center mb-8">
           <CircularImage color={color} />
         </div>
 
         {/* Texts */}
         <div className="mb-10">
-          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-6">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4">
             Hi, I'm Jihen Jabeur
           </h1>
           <p className="text-base sm:text-lg md:text-xl max-w-2xl mx-auto">
@@ -79,7 +79,7 @@ export default function Hero() {
         <AnimatedButton color={color} scrollToSection={scrollToSection} />
 
         {/* Social icons */}
-        <div className="mt-16 flex justify-center gap-12 sm:gap-20">
+        <div className="mt-16 flex justify-center gap-12 sm:gap-15">
           {[
             { Icon: FaGithub, href: "https://github.com/JihenJabeur02" },
             { Icon: FaLinkedin, href: "https://www.linkedin.com/in/jihen-jabeur-1170702a2/" },
